7e6ae210a5c76bbe2ae0482a7fdb678c88a1c977
Denise232jg <73981195+Denise232jg@users.noreply.github.com>
2020-11-05 02:23:05 -0600

    Update settings.py

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -74,19 +74,12 @@ WSGI_APPLICATION = 'Tienda.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'sql_server.pyodbc',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'Tienda',
-        'USER': 'sa',
-        'PASSWORD': 'sa',
-        'HOST': '',
-        'PORT': '',
-        'OPTIONS': {
-            'driver': 'ODBC Driver 13 for SQL Server',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
